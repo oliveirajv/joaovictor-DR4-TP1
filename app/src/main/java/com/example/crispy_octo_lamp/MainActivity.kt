a8330cbda2f599d157883cf1e5ce8b80edce4511
oliveirajv <johnwinolive@outlook.com>
2022-03-25 20:41:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/crispy_octo_lamp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.crispy_octo_lamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
